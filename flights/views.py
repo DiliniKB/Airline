21fa42e9d832218910f0c72e0c30e2d19bd23809
DiliniKB <84224505+DiliniKB@users.noreply.github.com>
2022-09-04 12:58:28 +0530

    error handeling

--- flights/views.py
+++ flights/views.py
@@ -15,21 +15,28 @@ def index(request):
 #get is used when it is exactly one result
 #pk is for primary key
 def flight(request, flight_id):
-    flight = Flight.objects.get(pk=flight_id)
-    passengers = flight.passenger.all()
-    return render(request, 'flights/flight.html', {
+    try:
+        flight = Flight.objects.get(id=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight not found.")
+    return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": passengers,
-        "non_passengers": Passenger.objects.exclude(flights=flight_id).all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 def book(request, flight_id):
     if request.method == 'POST':
-        flight = Flight.objects.get(pk=flight_id)
-        passenger = Passenger.objects.get(pk = (int(request.POST["passenger"])))
+        try:
+            passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+            flight = Flight.objects.get(pk=flight_id)
+        except KeyError:
+            return HttpResponseBadRequest("Bad Request: no flight chosen")
+        except Flight.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: flight does not exist")
+        except Passenger.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: passenger does not exist")
         passenger.flights.add(flight)
-
-        # comma after the args is compulsory
         return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
 
     flight = Flight.objects.get(id=flight_id)
